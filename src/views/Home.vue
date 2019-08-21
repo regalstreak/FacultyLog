17a2faf0e974c725d47fefbde07e66660952f9fb
Neil Agarwal <neil@neilagarwal.me>
2019-08-21 12:50:23 +0530

    views: Home basic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,24 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <h2>Welcome to FacultyLog</h2>
+    <v-layout column>
+      <v-flex class="button-flex">
+        <v-btn color="primary" to="/principal">Principal</v-btn>
+      </v-flex>
+      <v-flex class="button-flex">
+        <v-btn color="primary" to="/ttc">Timetable Coordinator</v-btn>
+      </v-flex>
+    </v-layout>
+
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="stylus" scoped>
+.button-flex {
+  margin: 20px 0;
+}
+</style>
